60aa62506bd7df230693f601b6872485aab342e4
xujess <160094328+xujess@users.noreply.github.com>
2024-02-15 05:34:42 -0800

    app.py

--- app.py
+++ app.py
@@ -82,10 +82,10 @@ if bids:  # 确保 bids 不为空
         for K in Ks:
           benchmark = weighted_sum * K
           benchmark = round(benchmark,6)
-          data.append([delta,A,round(0.95*A,6),lower_limit,C,b,weighted_sum,K,benchmark])
+          data.append([delta,A,round(0.95*A,6),C,b,weighted_sum,K,benchmark])
 
     df = pd.DataFrame(data, columns=[
-      'delta', 'A', '0.95A', 'lower_limit', 'C', 'B',
+      'delta', 'A', '0.95A', 'C', 'B',
       'weighted_sum', 'K', 'benchmark'])
 
 
