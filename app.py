0de569ac2fc3e52092a8e350a0db31ff4ca32d0c
xujess <160094328+xujess@users.noreply.github.com>
2024-02-14 20:56:12 -0800

    app.py

--- app.py
+++ app.py
@@ -24,41 +24,44 @@ Ks = [0.95,0.955,0.96,0.965,0.97,0.975,0.98]
 
 # 计算C值
 # 计算评标价平均值 和范围下限
-bid_mean = statistics.mean(bids)
-lower_limit = (bid_mean * 0.7 + control_price * 0.3) * 0.75
 
-# 过滤bids,得到在范围内的bids
-in_range_bids = [b for b in bids if b >= lower_limit]
+if bids:  # 确保 bids 不为空
+  bid_mean = statistics.mean(bids)
+  lower_limit = (bid_mean * 0.7 + control_price * 0.3) * 0.75
 
-# C=在规定范围内的最低评标价
-in_range_bids.sort()
-C = in_range_bids[0]
+  # 过滤bids,得到在范围内的bids
+  in_range_bids = [b for b in bids if b >= lower_limit]
 
-data = []
-for delta in deltas:
+  # 如果 in_range_bids 不为空，继续计算
+  if in_range_bids:
 
-  # A=招标控制价×（100%－下浮率Δ）
-  A = control_price * (1-delta)
-  A = round(A,2)
+    # C=在规定范围内的最低评标价
+    in_range_bids.sort()
+    C = in_range_bids[0]
 
-  B_range = [b for b in bids if C <= b < A*0.95]
+    data = []
+    for delta in deltas:
 
-  for b in B_range:
-    weighted_sum = 0.5*A + 0.3*b + 0.2*C
-    weighted_sum = round(weighted_sum,6)
+      # A=招标控制价×（100%－下浮率Δ）
+      A = control_price * (1-delta)
+      A = round(A,2)
 
-    for K in Ks:
-      benchmark = weighted_sum * K
-      benchmark = round(benchmark,6)
-      data.append([delta,A,round(0.95*A,6),C,b,weighted_sum,K,benchmark])
+      B_range = [b for b in bids if C <= b < A*0.95]
 
-df = pd.DataFrame(data, columns=[
-  'delta', 'A', '0.95A', 'C', 'B',
-  'weighted_sum', 'K', 'benchmark'])
+      for b in B_range:
+        weighted_sum = 0.5*A + 0.3*b + 0.2*C
+        weighted_sum = round(weighted_sum,6)
+
+        for K in Ks:
+          benchmark = weighted_sum * K
+          benchmark = round(benchmark,6)
+          data.append([delta,A,round(0.95*A,6),C,b,weighted_sum,K,benchmark])
+
+    df = pd.DataFrame(data, columns=[
+      'delta', 'A', '0.95A', 'C', 'B',
+      'weighted_sum', 'K', 'benchmark'])
 
 
-st.title("评标基准价=(A×50%＋B×30%＋C×20%)×K")
+    st.title("评标基准价=(A×50%＋B×30%＋C×20%)×K")
 
-# 输入UI
-
-st.table(df)
+    st.table(df)
